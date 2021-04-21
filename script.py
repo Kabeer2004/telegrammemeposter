007e9d1da13e53961516c9edb83625ebe4954e91
Kabeer Ahmed Merchant <59280736+Kabeer2004@users.noreply.github.com>
2021-04-21 17:14:55 +0530

    making public

--- script.py
+++ script.py
@@ -12,7 +12,7 @@ import schedule
 
 # -1001299298185
 
-reddit = praw.Reddit(client_id='AGwJY0JMlUn0ig', client_secret='1dDTVaQKVfKfA2DK_99XAWKdOEYrqg', user_agent='TelegramMemePoster')
+reddit = praw.Reddit(client_id='your-client-id', client_secret='your-client-secret', user_agent='your-user-agent')
 
 
 # get 10 hot posts from subreddit
@@ -20,7 +20,7 @@ def post_memes():
     hot_posts = reddit.subreddit('dankmemes').hot(limit=23)
     for post in hot_posts:
        post_string = post.title + "\n" + post.url
-       base_url = 'https://api.telegram.org/bot1702989043:AAE_KprvG8HEClU8sTg0Rape_Y2eYzM82_c/sendMessage?chat_id=-1001299298185&text="{}"'.format(post_string)
+       base_url = 'https://api.telegram.org/your-bot-id-here/sendMessage?chat_id=-1001299298185&text="{}"'.format(post_string)
        requests.get(base_url)
        time.sleep(3600)
        
